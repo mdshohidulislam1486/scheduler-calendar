70445f8d80ba8f410fc5ba4daf9a6ecb2e1bb48d
mdshohidulislam1486 <Shohidul Islam>
2022-01-22 23:22:56 +0600

    updated

--- src/Pages/Home/Home/CalendarComp.js
+++ src/Pages/Home/Home/CalendarComp.js
@@ -7,25 +7,69 @@ import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { Typography } from '@mui/material';
+import { Typography, Container, Box} from '@mui/material';
 
 
 const locales = {
     'en-Us': require('date-fns/locale/en-US')
 }
 
+const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales
+})
+
+const events =[
+    {
+        title:"Bit Meeting",
+        allDay: true,
+        start: new Date(2022,0,15),
+        end: new Date(2022,0,15)
+    },
+    {
+        title:"Last Meeting",
+        allDay: true,
+        start: new Date(2022,0,21),
+        end: new Date(2022,0,21)
+    },
+    {
+        title:"Birthday",
+        allDay: true,
+        start: new Date(2022,0,22),
+        end: new Date(2022,0,22)
+    },
+    {
+        title:"Marraige Ceremony",
+        allDay: true,
+        start: new Date(2022,0,2),
+        end: new Date(2022,0,2)
+    },
+]
+
 
 
 const CalendarComp = () => {
     
     return (
-        <>
+        <Container>
          <Typography variant='h5' sx={{mt:5}}>
              Calander 
          </Typography>
 
+         <Box>
+             <Calendar localizer={localizer} events={events} startAccessor='start' endAccessor='end'
+             style={{height:500, margin:'50px'}}
+             >
 
-        </>
+             </Calendar>
+
+         </Box>
+
+
+        </Container>
     );
 };
 

f78e90897fb19aca5321bd3d8fa7813364c6563b
mdshohidulislam1486 <Shohidul Islam>
2022-01-23 01:35:42 +0600

    updated

--- src/Pages/Home/Home/CalendarComp.js
+++ src/Pages/Home/Home/CalendarComp.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import format from "date-fns/format";
 import getDay from "date-fns/getDay";
 import parse from "date-fns/parse";
@@ -7,7 +7,17 @@ import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { Typography, Container, Box} from '@mui/material';
+import { Typography, Container, Box, Button} from '@mui/material';
+import "react-datepicker/dist/react-datepicker.css";
+import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
+import moment from "moment";
+import { DragDropContext } from "react-dnd";
+import HTML5Backend from "react-dnd-html5-backend";
+import ReactDOM from "react-dom";
+
+
+
+
 
 
 const locales = {
@@ -24,7 +34,7 @@ const localizer = dateFnsLocalizer({
 
 const events =[
     {
-        title:"Bit Meeting",
+        title:"Big Meeting",
         allDay: true,
         start: new Date(2022,0,15),
         end: new Date(2022,0,15)
@@ -52,6 +62,21 @@ const events =[
 
 
 const CalendarComp = () => {
+    const DragAndDropCalendar = withDragAndDrop(Calendar);
+
+    const [newEvent, setNewEvent] = useState({
+        title:'',
+        start:'',
+        end:''
+    })
+    const [allEvents, setAllEvents] = useState(events)
+
+    const handleAddEvent = (e) =>{
+        setAllEvents([...allEvents, newEvent])
+        document.getElementById('filed1').innerValue=''
+        e.preventDefault();
+    }
+
     
     return (
         <Container>
@@ -59,8 +84,32 @@ const CalendarComp = () => {
              Calander 
          </Typography>
 
+        <Box textAlign={'center'}>
+            <Typography>Add New Event</Typography>
+            <form onSubmit={handleAddEvent}>
+                <input id='field1' type="text" placeholder='Add Event' name="" id="" style={{width:'15%'}} 
+                value={newEvent.title}
+                onChange={(e) => setNewEvent({...newEvent, title: e.target.value})}
+                />
+                <DatePicker
+                id='field2' 
+                placeholderText='Start Date' style={{marginRight:'3em'}}
+                selected={newEvent.start}
+                onChange={(start) => setNewEvent({...newEvent, start})}
+                ></DatePicker>
+                <DatePicker
+                id='field3' 
+                placeholderText='End Date' style={{marginRight:'3em'}}
+                selected={newEvent.end}
+                onChange={(end) => setNewEvent({...newEvent, end})}
+                ></DatePicker>
+                <Button type='submit' contained sx={{mt:1}}> Add New Event</Button>
+            </form>
+
+        </Box>
+
          <Box>
-             <Calendar localizer={localizer} events={events} startAccessor='start' endAccessor='end'
+             <Calendar localizer={localizer} events={allEvents} startAccessor='start' endAccessor='end'
              style={{height:500, margin:'50px'}}
              >
 

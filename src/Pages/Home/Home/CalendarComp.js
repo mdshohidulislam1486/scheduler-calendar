a9c06961f4528d6d59a40a86ce343e5d78e8ab60
mdshohidulislam1486 <Shohidul Islam>
2022-01-24 22:33:56 +0600

    updated

--- src/Pages/Home/Home/CalendarComp.js
+++ src/Pages/Home/Home/CalendarComp.js
@@ -3,21 +3,18 @@ import format from "date-fns/format";
 import getDay from "date-fns/getDay";
 import parse from "date-fns/parse";
 import startOfWeek from "date-fns/startOfWeek";
-import { Calendar, dateFnsLocalizer, momentLocalizer, Views } from "react-big-calendar";
+import { Calendar, dateFnsLocalizer} from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { Typography, Container, Box, Button} from '@mui/material';
 import "react-datepicker/dist/react-datepicker.css";
 import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
+import "react-datepicker/dist/react-datepicker.css";
 
-import moment from "moment";
-import { DragDropContext } from "react-dnd";
-import HTML5Backend from "react-dnd-html5-backend";
-import ReactDOM from "react-dom";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
-import Select from "react-select";
+
 
 
 
@@ -48,7 +45,7 @@ const listofEvents =[
         title:"Big Meeting",
         allDay: true,
         start: new Date(2022,0,15),
-        end: new Date(2022,0,15)
+        end: new Date(2022,0,17)
     },
     {
         title:"Last Meeting",
@@ -70,13 +67,17 @@ const listofEvents =[
     },
 ]
 
-
+ const inputFields = {
+     width:"20rem",
+     margin:'.5rem 0',
+     border:'2px solid green'
+ }
 
 const CalendarComp = () => {
     const [events, setEvents] = useState(listofEvents);
     const [allEvents, setAllEvents] = useState(events)
 
-      const [showCalendarModal, setShowCalendarModal] = useState(true);
+    
       const [selectedDate, setSelectedDate] = useState(undefined);
      
 
@@ -97,12 +98,6 @@ const CalendarComp = () => {
       };
      
 
-    const filterViewChange = (selected) => {
-        var indexOfSelected = [];
-        selected.map((selection) =>
-          indexOfSelected.push(selection.index)
-        );
-      };
 
     const DragAndDropCalendar = withDragAndDrop(Calendar);
 
@@ -118,38 +113,33 @@ const CalendarComp = () => {
         e.preventDefault();
     }
 
-
-    const handleSelectEvent = () =>{
-        const newevent =  allEvents;   
-    }
-
-    
     return (
         <Container>
          <Typography variant='h5' sx={{mt:5}}>
              Calander 
          </Typography>
 
-        <Box textAlign={'center'}>
+        <Box  sx={{mt:5}}>
             <Typography>Add New Event</Typography>
             <form onSubmit={handleAddEvent}>
-                <input id='field1' type="text" placeholder='Add Event' name="" id="" style={{width:'15%'}} 
+                <input id='field1' type="text" placeholder='Add Event' name="" id="" style={inputFields} 
                 value={newEvent.title}
                 onChange={(e) => setNewEvent({...newEvent, title: e.target.value})}
                 />
                 <DatePicker
-                id='field2' 
-                placeholderText='Start Date' style={{marginRight:'3em'}}
+             
+                style={{marginBottom:'2em'}}
+                placeholderText='Start Date' 
                 selected={newEvent.start}
                 onChange={(start) => setNewEvent({...newEvent, start})}
                 ></DatePicker>
                 <DatePicker
-                id='field3' 
-                placeholderText='End Date' style={{marginRight:'3em'}}
+               
+                placeholderText='End Date' style={inputFields}
                 selected={newEvent.end}
                 onChange={(end) => setNewEvent({...newEvent, end})}
                 ></DatePicker>
-                <Button type='submit' contained sx={{mt:1}}> Add New Event</Button>
+                <Button type='submit'  variant="contained" sx={{mt:1}}> Add New Event</Button>
             </form>
 
         </Box>
@@ -161,7 +151,6 @@ const CalendarComp = () => {
             popup
             onEventDrop={moveEvent}
             onEventResize={moveEvent}
-            onSelectEvent={(events) => handleSelectEvent(events)}
             onSelectSlot={handleSelectSlot}
             localizer={localizer}
             events={allEvents}
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,16 +1,11 @@
 import { Box } from '@mui/material';
 import React from 'react';
 import CalendarComp from './CalendarComp';
-import MyCalendar from './MyCalendar';
-
-
-
 
 const Home = () => {
     return (
     <Box>
         <CalendarComp></CalendarComp>
-        
     </Box>
     );
 };
